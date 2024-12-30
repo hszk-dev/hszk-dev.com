fd406961156deb3df56eb429ba2ed83300423753
h.suzuki <h.suzuki@guncys.com>
2024-12-31 01:26:39 +0900

    📱  Refactor ProfileCard layout for improved design and responsiveness

--- frontend/app/profile/page.tsx
+++ frontend/app/profile/page.tsx
@@ -26,15 +26,30 @@ function ProfileCard({
     process.env.NEXT_PUBLIC_API_URL ?? "http://127.0.0.1:1337"
   }${photo.url}`;
   return (
-    <div>
-      <Image
-        src={imageUrl}
-        alt={photo.alternativeText || name}
-        width={500}
-        height={500}
-      />
-      <div>
-        <h3>{name}</h3>
+    <div className="px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto font-energetic">
+      <div className="relative max-w-screen-2xl m-auto">
+        <div className="relative flex w-full flex-col lg:flex-row items-center justify-center gap-4 lg:gap-12 flex-col lg:flex-row-reverse">
+          <div className="mb-10 lg:mb-0 lg:col-span-6 lg:col-start-8 w-full lg:w-1/2 ">
+            <h2 className="text-2xl font-bold sm:text-3xl">About Me</h2>
+            <nav className="grid lg:gap-10 mt-5 md:mt-10 gap-5" aria-label="Tabs" role="tablist">
+              <h3>{name}</h3>
+            </nav>
+          </div>
+          <div className=" w-full lg:w-1/2">
+            <div className="relative w-full">
+              <div id="tabs-with-card-1" className="tabpanel" aria-labelledby="tabs-with-card-item-1">
+                <Image
+                  src={imageUrl}
+                  alt={photo.alternativeText || name}
+                  width={500}
+                  height={500}
+                  className="shadow-xl shadow-gray-200 max-h-[550px] rounded-xl dark:shadow-gray-900/[.2]"
+                />
+              </div>
+            </div>
+          </div>
+          
+        </div>
       </div>
     </div>
   );
@@ -73,7 +88,6 @@ export default async function Profile() {
     <div>
       <h1>Profile</h1>
       <ProfileCard {...profile.data} />
-      
     </div>
   );
 }
